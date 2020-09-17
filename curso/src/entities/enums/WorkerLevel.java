3b5acc9a5220428917e810d4cc2e6c9aaffcdf9d
daniaraujoxx <danyeladearaujo@hotmail.com>
2020-09-17 17:51:37 -0300

    exercicio udemy

---
+++ curso/src/entities/enums/WorkerLevel.java
@@ -0,0 +1,13 @@
+package entities.enums;
+
+public class WorkerLevel {
+	
+	private Integer junior;
+	private Integer midLevel;
+	private Integer senior;
+	public static WorkerLevel valueOf(String workerLevel) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
